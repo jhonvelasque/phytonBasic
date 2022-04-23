bd490bbcf87414ee88ce40d2b071f6cf2cff28d1
JHON CARLOS JEAN PAUL VELASQUE DURAND <jhon.velasque@unmsm.edu.pe>
2022-04-23 13:56:24 -0500

    oganizando todo

--- historias_locas.py
+++ historias_locas.py
@@ -19,7 +19,9 @@ verbo1='jugar'
 verbo2='pelear'
 adj='feo '
 verbo3='pelear'
+#usando alt z 
 madlib =f" mi abuelito ,que {verbo1} 75 ainos y casi {verbo2} me  hoy : tu abuela es la mujer  mas {adj} ,asi que?. he {verbo3} por un momento y luego le he dicho!" 
 print(madlib)
+#utilixzando shift+alt +a
 madlib =f"""   mi abuelito ,que {verbo1} 75 ainos y casi {verbo2} me  hoy : tu abuela es la mujer  mas {adj} ,asi que?. he {verbo3} por un momento y luego le he dicho!"""
 print(madlib)
