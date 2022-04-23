be548f4f303c9c7b21aa007129dfa7c0dcd65872
JHON CARLOS JEAN PAUL VELASQUE DURAND <jhon.velasque@unmsm.edu.pe>
2022-04-23 12:38:47 -0500

    ejemplo pequenio antes del aplicativo

---
+++ historias_locas.py
@@ -0,0 +1,9 @@
+print('hola mundo')
+##INICIO DE COMO VAMOS HACER NUESTRO APLICATIVO DE HISTORIAS LOCAS go
+# concatedenar cadenas
+#supongamos que queremos crear una cadena que diga 
+#aprende a programasr con ___________________.
+
+texto='aprende a programar con'
+nombre='jhon'
+print(texto+' '+nombre)
